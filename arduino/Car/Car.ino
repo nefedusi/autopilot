a2681c626280355fc8c06e0d62dad8e1e644e044
nefedusi <nefedusi@gmail.com>
2018-05-15 14:15:43 +0700

    Added arduino files - for traggic lights and for development-period car (with usart messages logging)

---
+++ arduino/Car/Car.ino
@@ -0,0 +1,75 @@
+#include <AFMotor.h>
+AF_DCMotor motorL(1);
+AF_DCMotor motorR(2);
+int cmd;
+int prev_cmd = 0;
+int motor = 0; //0 for neutral, -1 for left, 1 for right
+int sign = 0;
+int spd = 0;
+int num_of_digits = 0;
+
+//ADVANTAGES:
+//momental command executing
+//defended from hackers :-) (from all inpredictable strings)
+
+//DISADVANTAGES:
+//non-beautiful work with motors (if motor == 1) {...}
+//code duplication
+//difficult code
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    cmd = Serial.read();
+    Serial.print("char='");
+    Serial.print((char)cmd);
+    Serial.print("', int=");
+    Serial.println(cmd);
+    
+    if (cmd == 'L') {
+      motor = -1;
+    } else if (cmd == 'R') {
+      motor = 1;
+    } else if (cmd == '+' && (prev_cmd == 'L' || prev_cmd == 'R')) {
+      sign = 1;
+      num_of_digits = 0;
+      spd = 0;
+    } else if (cmd == '-' && (prev_cmd == 'L' || prev_cmd == 'R')) {
+      sign = -1;
+      num_of_digits = 0;
+      spd = 0;
+    } else if (isdigit(cmd) && (prev_cmd == '+' || prev_cmd == '-' || (isdigit(prev_cmd) && num_of_digits < 3))) { //num_of_digits is not necessary here!
+      num_of_digits = num_of_digits + 1;
+      spd = spd * 10;
+      spd = spd + cmd - 48;
+      if (num_of_digits == 3)
+      {
+        if (motor == -1)
+        {
+          if (sign == 1)
+          {
+            motorL.run(FORWARD);
+          } else
+          {
+            motorL.run(BACKWARD);
+          }
+          motorL.setSpeed(spd);
+        } else if (motor == 1)
+        {
+          if (sign == 1)
+          {
+            motorR.run(FORWARD);
+          } else
+          {
+            motorR.run(BACKWARD);
+          }
+          motorR.setSpeed(spd);
+        }
+      }
+    }
+    prev_cmd = cmd;
+  }
+}
---
+++ arduino/TrafficLights/TrafficLights.ino
@@ -0,0 +1,37 @@
+int r = 2, y = 8, g = 12;
+
+void setup() {
+  pinMode(r, OUTPUT);
+  pinMode(y, OUTPUT);
+  pinMode(g, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(r, HIGH);
+  delay(6000);
+  digitalWrite(y, HIGH);
+  delay(2000);
+  digitalWrite(r, LOW);
+  digitalWrite(y, LOW);
+  
+  digitalWrite(g, HIGH);
+  delay(6000);  
+  digitalWrite(g, LOW);
+  delay(500);
+  digitalWrite(g, HIGH);
+  delay(500);
+  digitalWrite(g, LOW);
+  delay(500);
+  digitalWrite(g, HIGH);
+  delay(500);
+  digitalWrite(g, LOW);
+  delay(500);
+  digitalWrite(g, HIGH);
+  delay(500);
+  digitalWrite(g, LOW);
+
+  digitalWrite(y, HIGH);
+  delay(2000);
+  digitalWrite(y, LOW);
+}
